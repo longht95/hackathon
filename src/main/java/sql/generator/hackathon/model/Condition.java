a291ca698be78bddd6a8bab5e8895aecce415793
Q.Tuan NV <nvtuanq321@gmail.com>
2021-08-22 21:29:33 +0700

    Merge

--- src/main/java/sql/generator/hackathon/model/Condition.java
+++ src/main/java/sql/generator/hackathon/model/Condition.java
@@ -19,5 +19,13 @@ public class Condition {
 	public String expression;
 	public List<String> listRight;
 	public String function;
+
+	
+	public Condition(String left, String expression, String right) {
+		this.left = left;
+		this.expression = expression;
+		this.right = right;
+	}
+	
 	
 }
