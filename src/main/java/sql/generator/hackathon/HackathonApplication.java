7b8883ce965a4ffdbee382437270d746da4145a0
long.hotrieu@gmail.com <long.hotrieu@gmail.com>
2021-08-25 20:49:43 +0700

    config deploy tomcat

--- src/main/java/sql/generator/hackathon/HackathonApplication.java
+++ src/main/java/sql/generator/hackathon/HackathonApplication.java
@@ -2,24 +2,20 @@ package sql.generator.hackathon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import net.sf.jsqlparser.JSQLParserException;
-import net.sf.jsqlparser.parser.CCJSqlParserUtil;
-import net.sf.jsqlparser.statement.Statement;
-import net.sf.jsqlparser.statement.select.PlainSelect;
-import net.sf.jsqlparser.statement.select.Select;
-import sql.generator.hackathon.create.main.TestInsertDB;
 
 @SpringBootApplication
-public class HackathonApplication {
-	
+public class HackathonApplication extends SpringBootServletInitializer {
+
 	public static void main(String[] args) throws JSQLParserException {
-		Statement stmt = CCJSqlParserUtil.parse("SELECT * FROM MY_TABLE1 tb1 INNER JOIN MY_TABLE2 tb2 ON tb1.column1 = tb2.column2 WHERE tb1.aa = '1'");
-		PlainSelect plain = (PlainSelect) ((Select)stmt).getSelectBody();
-		System.out.println(plain.getJoins().get(0).getRightItem().getAlias());
-		System.out.println(plain.getJoins().get(0).getOnExpression());
-		System.out.println(plain.getWhere());
 		SpringApplication.run(HackathonApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(HackathonApplication.class);
+	}
 }
